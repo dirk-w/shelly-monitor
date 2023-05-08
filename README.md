d9def8f4d342d2f4b3d712002dd6c48da9e1b90f
Dirk Wichmann <6073095+dirk-w@users.noreply.github.com>
2023-05-08 21:05:21 +0200

    content of readme and remove unused code in ConfigProps

--- README.md
+++ README.md
@@ -2,7 +2,16 @@
 this is a sample project to show how easy it is to develop a spring boot application
 
 ## Details
-for more details see my blog under (Blog Shelly Monitor)[www.dwich.de]
+for more details see my blog under [Blog Shelly Monitor](https://dwich.de/?p=290)
 
 ## installation / running
 
+run the mavan clean verify task 
+then there is a jar file under target, this file is runable with `java  -jar shelly-monitor-0.0.1.jar`
+
+> __Note__: java 17 must be installed on your system
+
+## TODO
+- show how to configure from outside 
+- run inside docker container
+- show dev and prod profile
--- src/main/java/eu/dwich/tools/shellymonitor/service/CheckShellyService.java
+++ src/main/java/eu/dwich/tools/shellymonitor/service/CheckShellyService.java
@@ -5,6 +5,7 @@ import eu.dwich.tools.shellymonitor.obj.ShellyValues;
 import eu.dwich.tools.shellymonitor.props.ConfigProps;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 import java.text.SimpleDateFormat;
@@ -27,18 +28,26 @@ public class CheckShellyService {
 
   public void checkShellyIsSwitched(){
     log.debug("start checking shelly status, fetching URL: {}", configProps.getShellyUrl());
-    ShellyValues shVals = restTemplate.getForObject(configProps.getShellyUrl(), ShellyValues.class);
-    if(shVals.ison()){
-      log.debug("shelly is on, nothing to do");
-    } else {
-      String subject = "checking shelly failed on : [" + dateFormat.format(new Date()) + "] please check";
-      log.info("shelly is not on, sending E-Mail for notification");
-      this.emailService.sendSimpleMessage(
-        configProps.getNotificationFrom(),
-        configProps.getNotificationTo(),
-        subject,
-        configProps.getNotificationText());
+    try {
+      ShellyValues shVals = restTemplate.getForObject(configProps.getShellyUrl(), ShellyValues.class);
+      if(shVals != null && shVals.ison()){
+        log.debug("shelly is on, nothing to do");
+      } else {
+        String subject = "checking shelly failed on : [" + dateFormat.format(new Date()) + "] please check";
+        log.info("shelly is not on, sending E-Mail for notification");
+          sendMail(subject, configProps.getNotificationText());
+      }
+    } catch (ResourceAccessException e) {
+      sendMail("Failed with ResourceAccessException [" + dateFormat.format(new Date()) + "] please check", configProps.getNotificationText());
     }
     log.debug("checking shelly status finished");
+  }
+
+  private void sendMail(String pSubject, String pNotificationText) {
+    this.emailService.sendSimpleMessage(
+      configProps.getNotificationFrom(),
+      configProps.getNotificationTo(),
+      pSubject,
+      pNotificationText);
   }
 }
